e34f9a9ac90cb74334c0aaa93cee74171663b2d2
曹长喜 <941038274@qq.com>
2019-07-06 22:46:57 +0800

    优化了性能

--- src/辐射Online/xsp/src/游戏中通用事件检查.lua
+++ src/辐射Online/xsp/src/游戏中通用事件检查.lua
@@ -11,7 +11,7 @@ function 界面检查()
 		95, 0, 0, 0)
 	if #point1 ~= 0 then
 		sysLog("发现了确认按钮，我要点击了")
-		点击(1, point1[1].x, point1[1].y, 3 * 1000);
+		点击(1, point1[1].x, point1[1].y, 1 * 1000);
 		return;
 	end
 	
@@ -21,7 +21,7 @@ function 界面检查()
 		95, 0, 0, 0)
 	if #point2 ~= 0 then
 		sysLog("发现了向下小手，我要点击了")
-		点击(1, point1[1].x, point1[1].y, 3 * 1000);
+		点击(1, point2[1].x, point2[1].y + 100, 1 * 1000);
 		return;
 	end
 	
@@ -31,7 +31,7 @@ function 界面检查()
 		95, 0, 0, 0)
 	if #point3 ~= 0 then
 		sysLog("发现了向上小手，我要点击了")
-		点击(1, point3[1].x, point3[1].y, 3 * 1000);
+		点击(1, point3[1].x, point3[1].y - 100, 1 * 1000);
 		return;
 	end
 	
@@ -41,20 +41,40 @@ function 界面检查()
 		95, 0, 0, 0)
 	if #point4 ~= 0 then
 		sysLog("发现了免费，我要点击了")
-		点击(1, point4[1].x, point4[1].y, 3 * 1000);
+		点击(1, point4[1].x, point4[1].y, 1 * 1000);
 		return;
 	end
 	
 	--检查是否发现过场动画
-	local point = findColors({0, 0, 1600, 900},
-			"0|0|0x1c857c,-14|23|0x000000,10|23|0x000000,0|42|0x1c696b",
-			95, 0, 0, 0)
-		if #point ~= 0 then
-			sysLog("发现过场动画，我要点击了");
-			点击(1, point[1].x, point[1].y, 3 * 1000);
-			break;
-		end
+	local point5 = findColors({0, 0, 1600, 900},
+		"0|0|0x1c857c,-14|23|0x000000,10|23|0x000000,0|42|0x1c696b",
+		95, 0, 0, 0)
+	if #point5 ~= 0 then
+		sysLog("发现过场动画，我要点击了");
+		点击(1, point5[1].x, point5[1].y, 1 * 1000);
+		return;
+	end
 	
+	local point6 = findColors({0, 0, 1599, 899},
+		"0|0|0xf6f8f9,24|28|0xfefeff,82|26|0xfbfcfc,120|23|0xfafbfc",
+		95, 0, 0, 0)
+	if #point6 ~= 0 then
+		sysLog("发现离开关卡，我要点击了");
+		点击(1, point6[1].x, point6[1].y, 1 * 1000);
+		return;
+	end
+	
+	local point7 = findColors({0, 0, 1599, 899},
+		"0|0|0xf9fbfb,-31|-1|0xdde6e7,-37|27|0xbacccf,14|27|0xb9cbce",
+		95, 0, 0, 0)
+	if #point7 ~= 0 then
+		sysLog("发现离开关卡后的确定，我要点击了");
+		点击(1, point7[1].x, point7[1].y, 1 * 1000);
+		return;
+	end
+	
+	sysLog("我什么都没发现，等待下一步操作");
+	mSleep(3 * 1000);
 	keepScreen(false);
 	
 end
