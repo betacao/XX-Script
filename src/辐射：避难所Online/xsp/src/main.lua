85bee925f660cf6ae7d789e25e8adb7916e81ae7
曹长喜 <941038274@qq.com>
2019-07-09 00:10:11 +0800

    还没突破

--- src/辐射：避难所Online/xsp/src/main.lua
+++ src/辐射：避难所Online/xsp/src/main.lua
@@ -9,19 +9,32 @@ require("战斗中");
 require("离开关卡")
 require("通用")
 
-init("0", 1); --以当前应用 Home 键在右边初始化
 
-isRunning = appIsRunning("com.shandagames.shelter.yeshen"); --检测辐射：避难所Online是否在运行
-if isRunning == 0 then --如果没有运行
-	sysLog("辐射：避难所Online不在运行");
-	sysLog("正在启动");
-	runApp("com.shandagames.shelter.yeshen") --运行辐射：避难所Online
-	mSleep(10 * 1000);  --等待程序响应
-else
-	sysLog("辐射：避难所Online正在运行");
-	--如果程序在后台，要调用起来
---	if
+init("0", 1); --以当前应用 Home 键在右边初始化
+appid = "com.shandagames.shelter.yeshen";
+
+function 检查状态()
+	isRunning = appIsRunning(appid); --检测辐射：避难所Online是否在运行
+	if isRunning == 0 then --如果没有运行
+		sysLog("辐射：避难所Online不在运行");
+		sysLog("正在启动");
+		runApp(appid) --运行辐射：避难所Online
+		mSleep(10 * 1000);  --等待程序响应
+	else
+		sysLog("辐射：避难所Online正在运行");
+		--如果程序在后台，要调用起来
+		local isfront = isFrontApp(appid); --更新前台状态
+		if isfront ~= 1 then
+			sysLog("辐射：避难所Online不在前台运行 现在重新打开");
+			closeApp(appid)
+			mSleep(3 * 1000);  --等待程序响应
+			runApp(appid) --运行辐射：避难所Online
+			mSleep(10 * 1000);  --等待程序响应
+		end
+	end
 end
+
+检查状态();
 
 --开始游戏流程
 
